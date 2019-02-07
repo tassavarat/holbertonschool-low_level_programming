2f46a5fb524064ba968461afe95f5b0d899d1410
tassavarat <tassavarat@gmail.com>
2019-02-07 00:35:15 +0000

    Fixed comments for betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Demonstration of puts function
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
