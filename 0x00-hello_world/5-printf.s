de8a151fe65bdf027f717176e13817cacbe975f7
tassavarat <tassavarat@gmail.com>
2019-02-07 22:30:56 +0000

    Adding untracked

---
+++ 0x00-hello_world/5-printf.s
@@ -0,0 +1,27 @@
+	.file	"5-printf.c"
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"with proper grammar, but the outcome is a piece of art,"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
