77fab07550a7d6ea6871c0370d2b49fe6e3b1082
tassavarat <tassavarat@gmail.com>
2019-02-07 01:06:48 +0000

    First commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Demonstrates the printf function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
