36524dfcebaa9f21fa8eb34259b4d74238e18e7f
tassavarat <tassavarat@gmail.com>
2019-02-08 00:20:14 +0000

    Changed logical operator to &&

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /**
-   * main - Prints out entire alphabet except e and q using putchar
-    *
-     * Return: 0
-      */
+ * main - Prints out entire alphabet except e and q using putchar
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		if (alphabet == 'e' || alphabet == 'q')
-			alphabet++;
-		else
+		if (alphabet != 'e' && alphabet != 'q')
 			putchar(alphabet);
 	}
 	putchar('\n');
