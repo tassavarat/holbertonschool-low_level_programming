3d84a0fea5e816590dd247c32505c5fc98b3236f
tassavarat <tassavarat@gmail.com>
2019-02-08 00:09:18 +0000

    First commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+   * main - Prints out entire alphabet except e and q using putchar
+    *
+     * Return: 0
+      */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' || alphabet != 'q')
+			putchar(alphabet);
+		else
+			alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
