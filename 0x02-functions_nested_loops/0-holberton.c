22b7e1d3108d4ec809fd054d77e4a30e55661481
tassavarat <tassavarat@gmail.com>
2019-02-11 15:25:55 -0800

    First commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - Prints out "Holberton" character by character
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
