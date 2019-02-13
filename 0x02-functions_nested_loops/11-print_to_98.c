f4a0021742cc7352c58e68b673280d95bedb18f0
tassavarat <tassavarat@gmail.com>
2019-02-12 20:43:09 -0800

    First commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints natural numbers to 98
+ * @n: Number being evaluated
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%i", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%i", n);
+				printf(", ");
+			n--;
+		}
+	}
+	printf("\n");
+}
