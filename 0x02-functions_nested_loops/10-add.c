5c14b9301ca65dd85158d8cc3983763b2573d772
tassavarat <tassavarat@gmail.com>
2019-02-12 19:59:33 -0800

    First add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - Sum of two integers
+ * @n0: First number
+ * @n1: Second number
+ *
+ * Return: Sum of both variables
+ */
+int add(int n0, int n1)
+{
+	return (n0 + n1);
+}
