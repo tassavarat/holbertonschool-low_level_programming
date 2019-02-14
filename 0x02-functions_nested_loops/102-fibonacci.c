825397519901f8edb43712b827b3badf28c06e6d
tassavarat <tassavarat@gmail.com>
2019-02-13 19:27:25 -0800

    First commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main -
+ *
+ * Return:
+ */
+int main(void)
+{
+	int i;
+	long x = 1;
+	long y = 2;
+	int runX = 1;
+
+	printf("%ld, %ld,", x, y);
+	for (i = 0; i < 48; i++)
+	{
+		if (runX == 1)
+		{
+			x += y;
+			printf(" %ld", x);
+			runX = 0;
+		}
+		else
+		{
+			y += x;
+			printf(" %ld", y);
+			runX = 1;
+		}
+		if (i != 47)
+			printf(",");
+	}
+	printf("\n");
+	return (0);
+}
