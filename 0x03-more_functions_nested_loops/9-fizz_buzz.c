4dd6e1b7e87694e97b0d025590b4ba7b4eeb64b6
tassavarat <tassavarat@gmail.com>
2019-02-14 21:19:17 -0800

    First commit

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 1 to 100
+ *
+ * Multiples of 3 prints Fizz
+ * Miltiples of 5 print Buzz
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%i ", i);
+	}
+	printf("\n");
+	return (0);
+}
