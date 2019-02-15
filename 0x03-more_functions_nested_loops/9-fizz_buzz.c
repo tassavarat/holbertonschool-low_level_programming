37660874efa439efc80cf926159a28bf5f6ff218
tassavarat <tassavarat@gmail.com>
2019-02-14 21:25:58 -0800

    Added case where multiple of 3 and 5

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,9 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
