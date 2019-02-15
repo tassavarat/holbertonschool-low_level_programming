ef179e517ebea71f784c0563ed23557b4b6ba863
tassavarat <tassavarat@gmail.com>
2019-02-15 00:08:23 -0800

    First commit

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Prints the largest prime factor of 612852475143
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	int maxPrime, oddNum;
+
+	while (n % 2 == 0)
+	{
+		maxPrime = 2;
+		n /= 2;
+	}
+	for (oddNum = 3; oddNum <= sqrt(n); oddNum += 2)
+	{
+		while (n % oddNum == 0)
+		{
+			maxPrime = oddNum;
+			n /= oddNum;
+		}
+		oddNum += 2;
+	}
+	printf("%i\n", maxPrime);
+	return (0);
+}
