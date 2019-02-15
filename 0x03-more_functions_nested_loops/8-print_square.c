b48da821e9cb3774bb431d3758c68b68321c9c50
tassavarat <tassavarat@gmail.com>
2019-02-14 21:02:31 -0800

    First commit

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_square - Prints a square
+ * @size: Dimensions of square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
