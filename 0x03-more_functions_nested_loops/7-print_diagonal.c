043d04ff1f75210856f1991cfd2167dbbdc38957
tassavarat <tassavarat@gmail.com>
2019-02-14 19:49:30 -0800

    First commit

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Draws a diagonal line on the terminal
+ * @n: Amount of \ to be printed
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (j = 0; n > 0; n --, j++)
+		{
+			for (i = 0; i < j; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+}
