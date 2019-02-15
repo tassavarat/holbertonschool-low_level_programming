14cddbd4831fb0f95500871c82ce6d99256e5183
tassavarat <tassavarat@gmail.com>
2019-02-14 21:21:24 -0800

    Removed trailing space

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,13 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%i ", i);
+			printf("%i", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
