a2dde4b2f65c4ab45069177c260b3ee35eb3f30c
tassavarat <tassavarat@gmail.com>
2019-02-19 00:00:33 -0800

    First commit

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - Returns the length of a string
+ * @s: Variable being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * rev_string - Reverses a string
+ * @s: Array being reversed
+ */
+void rev_string(char *s)
+{
+	int i, len, tmp;
+
+	len = _strlen(s) - 1;
+	for (i = 0; i <= (len / 2); i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - i];
+		s[len - i] = tmp;
+	}
+}
