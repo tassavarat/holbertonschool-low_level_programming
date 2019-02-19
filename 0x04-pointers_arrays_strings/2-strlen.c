df2bead038116f16287bd857fa3f0c72faaaafea
tassavarat <tassavarat@gmail.com>
2019-02-18 18:33:18 -0800

    First commit

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Variable being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + ++i))
+		;
+	return (i);
+}
