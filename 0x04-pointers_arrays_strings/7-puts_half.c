6f4b5592bd780cd3706ca049398d07b2dc438084
tassavarat <tassavarat@gmail.com>
2019-02-19 13:43:46 -0800

    First commit

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Variable being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * puts_half - Prints half of string
+ * @str: String being evaluated
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = _strlen(str);
+	for (i = len / 2; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
