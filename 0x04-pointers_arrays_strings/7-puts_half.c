c98a5debdf5f81db8ec9ecf1c32901ed007dca93
tassavarat <tassavarat@gmail.com>
2019-02-19 14:09:01 -0800

    Removed if and implemented odd condition into for loop

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -23,9 +23,7 @@ void puts_half(char *str)
 	int i, len;
 
 	len = _strlen(str);
-	if (len % 2 != 0)
-		len += 1;
-	for (i = len / 2; i < len; i++)
+	for (i = (len + 1) / 2; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
