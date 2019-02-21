f3ec54196ba21a76fdbf77e88a5214d732947db6
tassavarat <tassavarat@gmail.com>
2019-02-21 13:51:42 -0800

    First commit

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: String being concatenated
+ * @src: String being concatenated
+ * @n: Number of bytes
+ *
+ * Return: Pointer to resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+		i++;
+	while (j < n)
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (dest);
+}
