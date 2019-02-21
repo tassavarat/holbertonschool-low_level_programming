8ad93ed7a55541d89e6cfbe5866f7b61a09bd330
tassavarat <tassavarat@gmail.com>
2019-02-20 20:27:59 -0800

    First commit

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _atoi - Convert string to int
+ * @s: String being coverted
+ *
+ * Return: Converted integer
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int neg = 0;
+	int num = 0;
+	int conv = 0;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+			neg++;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			conv = conv * 10 + s[i] - '0';
+			num = 1;
+		}
+		else if (num == 1 && (s[i] < '0' || s[i] > '9'))
+			break;
+		i++;
+	}
+	if (neg % 2 == 0)
+		return (conv);
+	else
+		return (-conv);
+}
