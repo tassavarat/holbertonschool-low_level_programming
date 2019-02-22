08293eb69cf896b5069b65ad1a696b960a3e87db
tassavarat <tassavarat@gmail.com>
2019-02-22 12:54:17 -0800

    First commit

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: Integer to be printed
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	for (i = 1000000000; i > 0; i /= 10)
+	{
+		if (n / i)
+		{
+			if ((n / i) % 10 < 0)
+				_putchar(-(n / i % 10) + '0');
+			else
+				_putchar((n / i % 10) + '0');
+		}
+		else if (n / i == 0 && i == 1)
+		{
+			_putchar(n / i % 10 + '0');
+		}
+	}
+}
