a2a50334010aeabb7e6309bcf9ed53fcd0b0e138
tassavarat <tassavarat@gmail.com>
2019-02-21 16:32:34 -0800

    Removed printf

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
  * _strncpy - Copies a string
  * @dest: Array of size 98
@@ -13,11 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-		printf("%i ", i);
-	}
-	printf("%i ", i);
 	while (i < n)
 		dest[i++] = '\0';
 	return (dest);
