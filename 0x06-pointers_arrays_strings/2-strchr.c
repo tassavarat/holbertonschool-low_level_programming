2151c8fd31b6a9a1a6cb5b5bf14ad852b31521c3
tassavarat <tassavarat@gmail.com>
2019-02-25 18:34:45 -0800

    Changed index iteration position

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * _strchr - Locates a character in a string
@@ -12,12 +11,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i++])
+	while (s[i])
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
+		++i;
 	}
 	return (0);
 }
