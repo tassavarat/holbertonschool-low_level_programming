d571652afc5ca519754a5491eceb3f25227fd26b
tassavarat <tassavarat@gmail.com>
2019-02-27 17:02:15 -0800

    First commit

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Pointer to string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
