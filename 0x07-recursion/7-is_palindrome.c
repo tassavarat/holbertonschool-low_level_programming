35d3162f2ce2c7951bc133ff18a94aa35ed2a75f
tassavarat <tassavarat@gmail.com>
2019-02-27 23:04:49 -0800

    First commit

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -17,17 +17,16 @@ int _strlen_recursion(char *s)
  * @s: String being evaluated
  * @len: Length of string
  * @i: Index
- * @j: Reverse Index
  *
  * Return: 1 if palindrome, 0 if not
  */
-int palindrome_test(char *s, int len, int i, int j)
+int palindrome_test(char *s, int len, int i)
 {
-	if (s[i] != s[j])
+	if (s[i] != s[len - i])
 		return (0);
 	else if (i == len / 2)
 		return (1);
-	return (palindrome_test(s, len, i + 1, j - 1));
+	return (palindrome_test(s, len, i + 1));
 }
 /**
  * is_palindrome - Checks if palindrome
@@ -37,7 +36,5 @@ int palindrome_test(char *s, int len, int i, int j)
  */
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
-
-	return (palindrome_test(s, len, 0, len - 1));
+	return (palindrome_test(s, _strlen_recursion(s) - 1, 0));
 }
