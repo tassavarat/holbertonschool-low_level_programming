94b3805b037fda9d3f570d2c429506ac9d867bd7
tassavarat <tassavarat@gmail.com>
2019-03-03 15:06:22 -0800

    Added __attribute__((unused))

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  *
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
