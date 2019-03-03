963741a406079fe122d43c6f8d2a3bfd5b701e82
tassavarat <tassavarat@gmail.com>
2019-03-03 11:33:47 -0800

    Removed std lib and merged _strlen code into _strcpy

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,5 @@
 #include "holberton.h"
-#include <stdio.h> /* Please delete me */
 
-/**
- * _strlen - Returns the length of a string
- * @s: Variable being evaluated
- *
- * Return: Length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
 /**
  * _strcpy - Copies string pointed to by src
  * @dest: Array being copied to
@@ -24,9 +9,12 @@ int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i, len;
+	int len;
+	int i = 0;
 
-	len = _strlen(src);
+	while (*s++)
+		i++;
+	len = i;
 	for (i = 0; i <= len; i++)
 		dest[i] = src[i];
 	return (dest);
