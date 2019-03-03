c8d1f5b3d9b1c6056bd1c2499bcad0784fed72dc
tassavarat <tassavarat@gmail.com>
2019-03-03 15:16:32 -0800

    First commit

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all received arguments
+ * @argc: Number of command line arguments
+ * @argv: Array containing command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; ++i)
+		printf("%s\n", argv[i]);
+	return (0);
+}
