d372bfab8492bbbbdd85a1d37a0734c0d0dbc318
tassavarat <tassavarat@gmail.com>
2019-03-03 20:57:35 -0800

    Added relational statement

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -12,24 +12,17 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int sum = 0;
-	int letter_check;
+	char *endptr;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; ++i)
 		{
-			if (argv[i])
+			sum += strtol(argv[i], &endptr, 10);
+			if ((*endptr >= 'A' && *endptr <= 'Z') || (*endptr >= 'a' && *endptr <= 'z'))
 			{
-				letter_check = atoi(argv[i]);
-				if (letter_check)
-				{
-					sum += letter_check;
-				}
-				else
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
 		}
 		printf("%d\n", sum);
