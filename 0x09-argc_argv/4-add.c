b7078cd165af14c96939575c6c48a828ae179d8e
tassavarat <tassavarat@gmail.com>
2019-03-03 17:38:27 -0800

    First commit

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Adds positive numbers
+ * @argc: Number of command line arguments
+ * @argv: Array containing command line arguments
+ *
+ * Return: 0, 1 if non-digits are passed
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	char *endptr;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; ++i)
+		{
+			sum += strtol(argv[i], &endptr, 10);
+		}
+		if (*endptr)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
