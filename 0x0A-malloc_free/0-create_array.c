5f560f58ffcf074c378add0a87e49a488cab79f9
tassavarat <tassavarat@gmail.com>
2019-03-05 13:30:54 -0800

    Checked if malloc fails

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -17,6 +17,8 @@ char *create_array(unsigned int size, char c)
 	if (!size)
 		return (NULL);
 	str = malloc(sizeof(char) * size);
+	if (!str)
+		return (NULL);
 	for (i = 0; i < size; ++i)
 		str[i] = c;
 	return (str);
