47d9aa69702d8c0a09f764609056331ba506cc57
tassavarat <tassavarat@gmail.com>
2019-03-05 16:40:26 -0800

    First commit

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Variable being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * _strdup - Returns pointer to newly allocated space which contains a string
+ * @str: String to be copied
+ *
+ * Return: Pointer to new string identical to str, NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int len = _strlen(str);
+	int i;
+
+	if (!str)
+		return (NULL);
+	s = malloc(sizeof(char) * len + 1);
+	if (!s)
+		return (NULL);
+	for (i = 0; i <= len; ++i)
+		*(s + i) = *(str + i);
+	return (s);
+}
