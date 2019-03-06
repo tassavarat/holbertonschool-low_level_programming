31310b5250ab0bbccd1f8a1ee6ec4b10935ad4fd
tassavarat <tassavarat@gmail.com>
2019-03-05 18:21:42 -0800

    First commit

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Concatenated string, NULL if empty or fail
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int len;
+	int i = 0;
+
+	if (!s1 || !s2)
+		return (NULL);
+	while (*(s1 + i))
+		++i;
+	len = i;
+	i = 0;
+	while (*(s2 + i))
+		++i;
+	len += i;
+	str = malloc(sizeof(char) * (len));
+	if (!str)
+		return (NULL);
+	for (i = 0; *s1; ++i)
+		*(str + i) = *(s1++);
+	for (; i <= len; ++i)
+		*(str + i) = *(s2++);
+	return (str);
+}
