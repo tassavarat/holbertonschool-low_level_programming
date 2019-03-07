09464b22c7b4cd730d9a54e46ffe80331352ce69
tassavarat <tassavarat@gmail.com>
2019-03-07 13:51:06 -0800

    First commit

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Amount of memory being allocated
+ *
+ * Return: Pointer to allocated memory, 98 if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *n;
+
+	n = malloc(b);
+	if (!n)
+		exit (98);
+	return (n);
+}
