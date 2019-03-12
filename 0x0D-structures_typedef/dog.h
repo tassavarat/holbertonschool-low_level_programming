52e4f75cacd2ea3846bb6a3e7ad11a29adba940b
tassavarat <tassavarat@gmail.com>
2019-03-11 19:27:28 -0700

    Added final prototypes

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -14,4 +14,7 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* _DOG_H_ */
