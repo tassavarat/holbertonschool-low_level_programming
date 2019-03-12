d9280e753caa3489ac755ca18b550819513c2a29
tassavarat <tassavarat@gmail.com>
2019-03-11 19:12:28 -0700

    First commit

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialises a variable of type struct dog
+ * @d: Struct dog variable
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
