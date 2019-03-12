90b5d037448c879b0d3a10f12bb8697e45c66694
tassavarat <tassavarat@gmail.com>
2019-03-11 21:19:27 -0700

    Defined new type dog_t

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: Age of dog
  * @owner: Owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* _DOG_H_ */
