312ef88bdf1c7095497ceef805da66f8b68fb33a
tassavarat <tassavarat@gmail.com>
2019-03-11 20:02:10 -0700

    Removed precision

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -16,5 +16,5 @@ void print_dog(struct dog *d)
 	if (!d->owner)
 		printf("Owner: (nil)\n");
 	else
-		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
