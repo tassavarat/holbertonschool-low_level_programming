08c3a8fb68af6d5fd8ee3e945e8706b274b4c538
tassavarat <tassavarat@gmail.com>
2019-03-11 18:31:02 -0700

    First commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Information about dogs
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DOG_H_ */
