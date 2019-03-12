7b33752174c44ffac55b2e25e7ae8fdcad4c090e
tassavarat <tassavarat@gmail.com>
2019-03-11 22:19:55 -0700

    malloc for name and owner

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - Finds length of string
+ * @s: String being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		++i;
+	return (i);
+}
+/**
  * new_dog - Creates a new dog
  * @name: Name of dog
  * @age: Age of dog
@@ -12,12 +26,26 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int name_len, owner_len, i;
 
 	d = malloc(sizeof(struct dog));
 	if (!d)
 		return (NULL);
-	d->name = name;
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
+
+	d->name = malloc(sizeof(name_len));
+	if (!(d->name))
+		return (NULL);
+	for (i = 0; i <= name_len; ++i)
+		d->name[i] = name[i];
+
 	d->age = age;
-	d->owner = owner;
+
+	d->owner = malloc(sizeof(owner_len));
+	if (!(d->owner))
+		return (NULL);
+	for (i = 0; i <= owner_len; ++i)
+		d->owner[i] = name[i];
 	return (d);
 }
