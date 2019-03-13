6e74504ddc8f8a74450900dc587e5126c2acad15
tassavarat <tassavarat@gmail.com>
2019-03-13 15:53:21 -0700

    Shortened code and fixed no matches logic

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -17,13 +17,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && cmp)
 	{
 		for (i = 0; i < size; ++i)
-		{
 			if (cmp(*(array + i)))
-				break;
-		}
-		if (!i)
+				return (i);
+		if (i == size)
 			return (-1);
-		return (i);
 	}
 	return (-1);
 }
