d2a17c12f9731951ad721a3e6f3157f6ce3dd4dc
tassavarat <tassavarat@gmail.com>
2019-03-13 15:40:26 -0700

    Added statement for null array and cmp

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,12 +12,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; ++i)
-		if (cmp(*(array + i)))
-			break;
-	if (!i)
-		return (-1);
-	return (i);
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; ++i)
+			if (cmp(*(array + i)))
+				break;
+		if (!i)
+			return (-1);
+		return (i);
+	}
+	return (-1);
 }
