a691a90f94f5aa5fa7d403c089adafaf5be38285
tassavarat <tassavarat@gmail.com>
2019-03-13 15:34:48 -0700

    First commit

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer
+ * @array: Pointer to array
+ * @size: Size of array
+ * @cmp: Pointer to function code
+ *
+ * Return: Index of first match, -1 if size <=0 or no matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; ++i)
+		if (cmp(*(array + i)))
+			break;
+	if (!i)
+		return (-1);
+	return (i);
+}
