059cafc61e42b2ad4c82db72921a3a06666053a1
tassavarat <tassavarat@gmail.com>
2019-03-13 14:47:08 -0700

    Updated prototypes for problem 2

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -5,4 +5,5 @@
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* _FUNCTION_POINTERS_H_ */
