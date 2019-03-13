fa6f8f0cfbe2af132dbfbddb5875ec6ea0b439e0
tassavarat <tassavarat@gmail.com>
2019-03-13 15:42:00 -0700

    Moved if statement

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,10 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0)
+		return (-1);
 	if (array && cmp)
 	{
-		if (size <= 0)
-			return (-1);
 		for (i = 0; i < size; ++i)
 			if (cmp(*(array + i)))
 				break;
