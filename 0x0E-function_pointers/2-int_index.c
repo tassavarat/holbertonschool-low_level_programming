1e00d078b42f1f4220352b11eb6706b620f0f518
tassavarat <tassavarat@gmail.com>
2019-03-14 11:09:14 -0700

    Removed unnecessary if statements

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,15 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
 	if (array && cmp)
 	{
 		for (i = 0; i < size; ++i)
 			if (cmp(*(array + i)))
 				return (i);
-		if (i == size)
-			return (-1);
 	}
 	return (-1);
 }
