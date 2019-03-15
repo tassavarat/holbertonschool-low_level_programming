366da7617897010edbe945c540862af9071f4ac8
tassavarat <tassavarat@gmail.com>
2019-03-14 17:03:11 -0700

    Added stdlib to header

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define _FUNCTION_POINTERS_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
