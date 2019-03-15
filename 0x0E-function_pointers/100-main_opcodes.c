5899506a3879470ec2a4a079011027eecc19d130
tassavarat <tassavarat@gmail.com>
2019-03-14 17:01:25 -0700

    First commit

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+
+/**
+ * main - Prints the opcodes of main function
+ * @argc: Number of command line arguments
+ * @argv: Array containing command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int byte, i;
+
+	byte = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (byte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < byte; i++)
+	{
+		printf("%02x", ((unsigned char *)main)[i]);
+		if (i < byte - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
