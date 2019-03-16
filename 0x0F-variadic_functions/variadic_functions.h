aa81f3d6782f511b5c0cea0ffc4cc88f37561f9f
tassavarat <tassavarat@gmail.com>
2019-03-15 21:06:11 -0700

    Added struct, typedef, headers

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,8 +1,21 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-	
+
+/**
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+} print_type;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
