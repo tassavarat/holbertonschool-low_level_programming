4ef3b75fc84d6d075e81582f4bc90994d7b8950f
tassavarat <tassavarat@gmail.com>
2019-03-21 16:06:03 -0700

    Added comments

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_list - Prints all elements of a list
+ * @h: Pointer to a list
+ *
+ * Return: Number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
