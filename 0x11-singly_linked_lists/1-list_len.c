649c8b2ba8898c7edd9bd2401cbe8e78eaab9b61
tassavarat <tassavarat@gmail.com>
2019-03-21 16:28:03 -0700

    First commit

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * List_len - Returns number of elements in linked list
+ * @h: Pointer to a list
+ *
+ * Return: Number of elements in linked list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		++i;
+	}
+	return (i);
+}
