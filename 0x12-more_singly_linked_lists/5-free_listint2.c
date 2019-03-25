4f7854fb123b18cc587aa11fb4763b2b64356617
tassavarat <tassavarat@gmail.com>
2019-03-25 14:06:14 -0700

    First commit

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list
+ * @head: Pointer to address of a list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
