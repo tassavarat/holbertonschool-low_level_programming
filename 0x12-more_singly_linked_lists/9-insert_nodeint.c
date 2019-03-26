7faa74f90cbdbe10bbb263091ad7921f4d6be257
tassavarat <tassavarat@gmail.com>
2019-03-26 15:15:10 -0700

    Returns NULL if idx is larger than amount of nodes

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	while (*head && idx)
 	{
+		if (!tmp)
+		{
+			printf("DADDY\n");
+			return (NULL);
+		}
 		if (i == idx - 1)
 			break;
 		tmp = tmp->next;
