b42a87f606c44973ad8439d87bcb899b6fe568b6
tassavarat <tassavarat@gmail.com>
2019-03-26 13:30:16 -0700

    Rewrote code to be safer by initialising num to 0

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * pop_listint - Deletes the head node of a linked list
- * Pointer to a struct
+ * @head: Pointer to a struct
  *
  * Return: Head node's data (n)
  * 0 if linked list is empty
@@ -10,12 +10,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
-	int num = (*head)->n;
+	int num = 0;
 
-	if (!*head)
-		return (0);
+	if (*head)
+	{
 	tmp = (*head)->next;
 	free(*head);
 	*head = tmp;
-	return (num);	
+	}
+		return (num);
 }
