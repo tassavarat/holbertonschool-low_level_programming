cae68bb6470783282b43367aa86a0a482650ae77
tassavarat <tassavarat@gmail.com>
2019-03-28 18:29:56 -0700

    First commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index
+ * @n: Bit being evaluated
+ * @index: Index, starting from 0 of the bit we want
+ *
+ * Return: Value of the bit at index
+ * -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	else
+		return (n >> index & 1);
+}
