392c175081eaad8f08875755c084b84f73170a06
tassavarat <tassavarat@gmail.com>
2019-04-02 13:00:38 -0700

    Added unistd header

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
