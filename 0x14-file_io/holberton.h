2a0a68f1e1cb2746592f25f6b8a4d16a1909d6b2
tassavarat <tassavarat@gmail.com>
2019-04-02 14:54:35 -0700

    Added header files for I/O calls

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -2,8 +2,11 @@
 #define HOLBERTON_H
 
 #include <stdlib.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
