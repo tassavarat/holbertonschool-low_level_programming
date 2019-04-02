63b0389bb07cbe4ef5db210e365d256cdee39006
tassavarat <tassavarat@gmail.com>
2019-04-02 16:30:00 -0700

    Fixed formatting

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		return (1);
 	if (write(fd, text_content, _strlen(text_content)) == -1)
-			return (-1);
+		return (-1);
 	close(fd);
 	return (1);
 }
