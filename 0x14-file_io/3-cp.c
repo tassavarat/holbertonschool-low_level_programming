b9dd0c3c4da3aaa00e99b30f0d4d71e0e75ff552
tassavarat <tassavarat@gmail.com>
2019-04-02 18:59:46 -0700

    Condensed code and added additional exit statement for write failure

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -10,13 +10,10 @@
 int main(int argc, char *argv[])
 {
 	int f1, f2, n;
-	char buf[BUFSIZ];
+	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	f1 = open(argv[1], O_RDONLY);
 	if (f1 == -1)
 	{
@@ -29,17 +26,16 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-	while ((n = read(f1, buf, BUFSIZ)) > 0)
+	while ((n = read(f1, buf, 1024)) > 0)
 		write(f2, buf, n);
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(f1) > 0)
-	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", f1);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", f1), exit(100);
 	if (close(f2) > 0)
-	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", f2);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", f2), exit(100);
 	return (0);
 }
