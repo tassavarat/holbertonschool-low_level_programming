ad61bed88ae138a17d3c344fa7d6cd8aaca80c72
tassavarat <tassavarat@gmail.com>
2019-05-04 14:12:52 -0700

    Added conditional for NULL

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -18,6 +18,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
+	if (*head)
+		(*head)->prev = new;
 	*head = new;
 
 	return (new);
