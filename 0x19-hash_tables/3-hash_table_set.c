70b8a5bd7f6f6d26eebea4b4e85024751e48e250
tassavarat <tassavarat@gmail.com>
2019-06-01 23:32:06 -0700

    Added strdup fail for key

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -37,6 +37,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new)
 		return (0);
 	new->key = strdup(key);
+	if (!new->key)
+		return (0);
 	new->value = strdup(value);
 	if (!new->value)
 		return (0);
