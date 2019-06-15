7c7ba16a5c4e37dca977f0063c9c539824e7ef38
tassavarat <tassavarat@gmail.com>
2019-06-15 16:16:05 -0700

    Refactored code

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -10,25 +10,17 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	int sortperf, val;
 
-	if (array)
-	{
-		sortperf = 1;
-		val = 0;
+	sortperf = 1;
+	val = 0;
 
-		while (sortperf)
-		{
-			sortperf = 0;
-			for (i = 0; i < size; ++i)
+	while (array && sortperf)
+		for (i = 0, sortperf = 0; i < size; ++i)
+			if (array[i + 1] && array[i] > array[i + 1])
 			{
-				if (array[i + 1] && array[i] > array[i + 1])
-				{
-					val = array[i];
-					array[i] = array[i + 1];
-					array[i + 1] = val;
-					print_array(array, size);
-					sortperf = 1;
-				}
+				val = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = val;
+				print_array(array, size);
+				sortperf = 1;
 			}
-		}
-	}
 }
