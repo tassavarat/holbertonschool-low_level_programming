b4bb55935c98b1736a229ae7b50acd7fbab9f978
tassavarat <tassavarat@gmail.com>
2019-06-15 16:32:24 -0700

    Removed printf statements

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -15,15 +15,6 @@ void insertion_sort_list(listint_t **list)
 		cur_tmp = current->next;
 		while (tmp && current->n < tmp->n)
 		{
-			/*
-			printf("#####################################\n");
-			printf("##current->n is %d##\n", current->n);
-			printf("##tmp->n is %d##\n", tmp->n);
-			if (cur_tmp)
-				printf("##cur_tmp->n is %d##\n", cur_tmp->n);
-			else
-				printf("cur_tmp is NULL\n");
-			*/
 			if (tmp->prev)
 				tmp->prev->next = current;
 			else
@@ -37,17 +28,6 @@ void insertion_sort_list(listint_t **list)
 			print_list(*list);
 			tmp = current->prev;
 			cur_tmp = current->next;
-			/*
-			printf("##after current->n is %d##\n", current->n);
-			if (tmp)
-				printf("##after tmp->n is %d##\n", tmp->n);
-			else
-				printf("tmp is NULL\n");
-			if (cur_tmp)
-				printf("##after cur_tmp->n is %d##\n", cur_tmp->n);
-			else
-				printf("cur_tmp is NULL\n");
-			*/
 		}
 		current = cur_tmp;
 	}
