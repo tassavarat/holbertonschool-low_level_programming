9a82885640dacd82a2a962eb17aa80c364ba9090
tassavarat <tassavarat@gmail.com>
2019-06-16 15:44:39 -0700

    Header update and implemented selection sort

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * swapp - Swaps specified array elements then prints array
+ * @array: Array to swap elements
+ * @size: Size of array
+ * @i: First index to swap
+ * @j: Second index to swap
+ * @val: Value to save
+ */
+void swapp(int *array, size_t size, size_t i, int j, int val)
+{
+	array[j] = array[i];
+	array[i] = val;
+	print_array(array, size);
+}
+
+/**
+ * selection_sort - Selection sort algorithm
+ * @array: Array to sort
+ * @size: Size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t si, i;
+	int min, min_pos, min_update;
+
+	min = array[0];
+	min_update = 0;
+	for (si = 0; array && si < size; ++si, min = array[si], min_update = 0)
+	{
+		for (i = si; i < size; ++i)
+			if (min > array[i])
+			{
+				min = array[i];
+				min_pos = i;
+				min_update = 1;
+			}
+		if (min_update)
+			swapp(array, size, si, min_pos, min);
+	}
+}
