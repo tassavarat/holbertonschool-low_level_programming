797cef1a703d51fbe835f4cfa72550e27409b94f
tassavarat <tassavarat@gmail.com>
2019-06-16 15:44:08 -0700

    Added swapp function and fixed edge cases

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swapp - Swaps specified array elements then prints array
+ * @array: Array to swap elements
+ * @size: Size of array
+ * @i: First index to swap
+ * @j: Second index to swap
+ * @val: Value to save
+ */
+void swapp(int *array, size_t size, size_t i, int j, int val)
+{
+	array[j] = array[i];
+	array[i] = val;
+	print_array(array, size);
+}
+
+/**
  * bubble_sort - Bubble sort algorithm
  * @array: Array to sort
  * @size: Size of array
@@ -13,13 +28,13 @@ void bubble_sort(int *array, size_t size)
 	sortperf = 1;
 	val = 0;
 	while (array && sortperf)
-		for (i = 0, sortperf = 0; i < size; ++i)
-			if (array[i + 1] && array[i] > array[i + 1])
+		for (i = 0, sortperf = 0; i + 1 < size && array[i + 1]; ++i)
+		{
+			if (array[i] > array[i + 1])
 			{
-				val = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = val;
-				print_array(array, size);
+				val = array[i + 1];
+				swapp(array, size, i, i + 1, val);
 				sortperf = 1;
 			}
+		}
 }
--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -8,27 +8,30 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *tmp, *cur_tmp;
 
-	current = (*list)->next;
-	while (current)
+	if (list)
 	{
-		tmp = current->prev;
-		cur_tmp = current->next;
-		while (tmp && current->n < tmp->n)
+		current = (*list)->next;
+		while (current)
 		{
-			if (tmp->prev)
-				tmp->prev->next = current;
-			else
-				(*list) = current;
-			current->next = tmp;
-			tmp->next = cur_tmp;
-			if (cur_tmp)
-				cur_tmp->prev = tmp;
-			current->prev = tmp->prev;
-			tmp->prev = current;
-			print_list(*list);
 			tmp = current->prev;
 			cur_tmp = current->next;
+			while (tmp && current->n < tmp->n)
+			{
+				if (tmp->prev)
+					tmp->prev->next = current;
+				else
+					(*list) = current;
+				current->next = tmp;
+				tmp->next = cur_tmp;
+				if (cur_tmp)
+					cur_tmp->prev = tmp;
+				current->prev = tmp->prev;
+				tmp->prev = current;
+				print_list(*list);
+				tmp = current->prev;
+				cur_tmp = current->next;
+			}
+			current = cur_tmp;
 		}
-		current = cur_tmp;
 	}
 }
