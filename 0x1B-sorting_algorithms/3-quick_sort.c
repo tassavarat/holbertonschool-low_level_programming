ea58a0d432e849b518f84063666f797aa9ad62e1
tassavarat <tassavarat@gmail.com>
2019-06-17 21:24:26 -0700

    Added quicksort algorithm

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * swapp - Swaps specified array elements then prints array
+ * @array: Array to swap elements
+ * @size: Size of array
+ * @i: First index to swap
+ * @j: Second index to swap
+ * @val: Value to save
+ */
+void swapp(int *array, size_t size, size_t i, int j, int val)
+{
+	array[j] = array[i];
+	array[i] = val;
+	print_array(array, size);
+}
+
+/**
+ * partition - Partitions array based on pivot
+ * @array: Array to sort
+ * @size: Size of array
+ * @lo: Start index
+ * @hi: End index
+ *
+ * Return: Position of partition
+ */
+int partition(int *array, size_t size, int lo, int hi)
+{
+	int pivot, i, j;
+
+	pivot = array[hi];
+	i = lo - 1;
+	for (j = lo; j < hi; ++j)
+	{
+		if (array[j] < pivot)
+		{
+			++i;
+			if (i != j)
+				swapp(array, size, i, j, array[j]);
+		}
+	}
+	++i;
+	if (array[i] != array[hi])
+		swapp(array, size, i, hi, array[hi]);
+	return (i);
+}
+
+/**
+ * quicksort - Recursion funtion
+ * @array: Array to sway elements
+ * @size: Size of array
+ * @lo: Start index
+ * @hi: End index
+ */
+void quicksort(int *array, size_t size, int lo, int hi)
+{
+	int p;
+
+	if  (lo < hi)
+	{
+		p = partition(array, size, lo, hi);
+		quicksort(array, size, lo, p - 1);
+		quicksort(array, size, p + 1, hi);
+	}
+}
+
+/**
+ * quick_sort - Quick sort algorithm
+ * @array: Array to swap elements
+ * @size: Size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array)
+		quicksort(array, size, 0, size - 1);
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -23,5 +23,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
