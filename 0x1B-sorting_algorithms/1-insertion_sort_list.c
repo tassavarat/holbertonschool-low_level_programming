40ff9d4b23439bcadeb3f39b868809fb2c311dd5
tassavarat <tassavarat@gmail.com>
2019-06-18 22:45:36 -0700

    Refactored code

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -2,30 +2,23 @@
 
 /**
  * swapp_list - Swaps linked list nodes then prints list
- * @list: Linked list to sort
- * @current: Current node
- * @tmp: Node before current
- * @cur_tmp: Node after current
+ * @list: Linked list to swap
+ * @n1: First node
+ * @n2: Second node
  */
-void swapp_list(listint_t **list, listint_t *current, listint_t *tmp,
-		listint_t *cur_tmp)
+void swapp_list (listint_t **list, listint_t *n1, listint_t *n2)
 {
-	while (tmp && current->n < tmp->n)
-	{
-		if (tmp->prev)
-			tmp->prev->next = current;
-		else
-			(*list) = current;
-		current->next = tmp;
-		tmp->next = cur_tmp;
-		if (cur_tmp)
-			cur_tmp->prev = tmp;
-		current->prev = tmp->prev;
-		tmp->prev = current;
-		print_list(*list);
-		tmp = current->prev;
-		cur_tmp = current->next;
-	}
+	if (n1->prev)
+		n1->prev->next = n2;
+	else
+		(*list) = n2;
+	if (n2->next)
+		n2->next->prev = n1;
+	n2->prev = n1->prev;
+	n1->next = n2->next;
+	n2->next = n1;
+	n1->prev = n2;
+	print_list(*list);
 }
 /**
  * insertion_sort_list - Insertion sort algorithm
@@ -33,17 +26,17 @@ void swapp_list(listint_t **list, listint_t *current, listint_t *tmp,
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *tmp, *cur_tmp;
+	listint_t *current, *tmp;
 
 	if (list)
 	{
 		current = (*list)->next;
 		while (current)
 		{
-			tmp = current->prev;
-			cur_tmp = current->next;
-			swapp_list(*&list, current, tmp, cur_tmp);
-			current = cur_tmp;
+			tmp = current;
+			while (tmp->prev && current->n < tmp->prev->n)
+				swapp_list(*&list, tmp->prev, tmp);
+			current = current->next;
 		}
 	}
 }
