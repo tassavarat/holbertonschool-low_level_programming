780a0adb86c89391062a7c7f3303223f10ce564c
tassavarat <tassavarat@gmail.com>
2019-06-18 17:13:57 -0700

    Only swap and print if values are different

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -7,16 +7,24 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int tmp;
+	int tmp, swp;
 	size_t i, j, gap;
 
 	if (array)
 	{
 		for (gap = 1; gap * 3 + 1 < size; gap = gap * 3 + 1)
 			;
-		for (; gap; gap = (gap - 1) / 3, print_array(array, size))
+		for (swp = 0; gap; gap = (gap - 1) / 3)
+		{
 			for (i = gap; i < size; ++i, array[j] = tmp)
 				for (j = i, tmp = array[i]; j >= gap && array[j - gap] > tmp; j -= gap)
-					array[j] = array[j - gap];
+					if (array[j] != array[j - gap])
+					{
+						array[j] = array[j - gap];
+						swp = 1;
+					}
+			if (swp)
+				print_array(array, size);
+		}
 	}
 }
