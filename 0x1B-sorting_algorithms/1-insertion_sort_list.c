837ad2c1ec93405cc0fd58e12c634eb1150e154b
tassavarat <tassavarat@gmail.com>
2019-06-18 20:20:10 -0700

    Added swapp_list function

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,5 +1,24 @@
 #include "sort.h"
 
+void swapp_list(listint_t **list, listint_t *current, listint_t *tmp, listint_t *cur_tmp)
+{
+	while (tmp && current->n < tmp->n)
+	{
+		if (tmp->prev)
+			tmp->prev->next = current;
+		else
+			(*list) = current;
+		current->next = tmp;
+		tmp->next = cur_tmp;
+		if (cur_tmp)
+			cur_tmp->prev = tmp;
+		current->prev = tmp->prev;
+		tmp->prev = current;
+		print_list(*list);
+		tmp = current->prev;
+		cur_tmp = current->next;
+	}
+}
 /**
  * insertion_sort_list - Insertion sort algorithm
  * @list: Linked list to sort
@@ -15,22 +34,7 @@ void insertion_sort_list(listint_t **list)
 		{
 			tmp = current->prev;
 			cur_tmp = current->next;
-			while (tmp && current->n < tmp->n)
-			{
-				if (tmp->prev)
-					tmp->prev->next = current;
-				else
-					(*list) = current;
-				current->next = tmp;
-				tmp->next = cur_tmp;
-				if (cur_tmp)
-					cur_tmp->prev = tmp;
-				current->prev = tmp->prev;
-				tmp->prev = current;
-				print_list(*list);
-				tmp = current->prev;
-				cur_tmp = current->next;
-			}
+			swapp_list(*&list, current, tmp, cur_tmp);
 			current = cur_tmp;
 		}
 	}
