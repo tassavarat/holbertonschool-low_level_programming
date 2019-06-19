55c33d3b18aaf5fc4b0d376fec715b318eec3a82
tassavarat <tassavarat@gmail.com>
2019-06-18 20:22:09 -0700

    Added swapp_list function

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,5 +1,12 @@
 #include "sort.h"
 
+/**
+ * swapp_list - Swaps linked list nodes then prints list
+ * @list: Linked list to sort
+ * @current: Current node
+ * @tmp: Node before current
+ * @cur_tmp: Node after current
+ */
 void swapp_list(listint_t **list, listint_t *current, listint_t *tmp, listint_t *cur_tmp)
 {
 	while (tmp && current->n < tmp->n)
