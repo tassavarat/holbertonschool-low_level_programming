3a47154306a8c7823622fd902529cfcbea8974b6
tassavarat <tassavarat@gmail.com>
2019-06-18 17:39:53 -0700

    Updated print logic

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -7,23 +7,26 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int tmp, swp;
+	int tmp, print;
 	size_t i, j, gap;
 
+	print = 0;
 	if (array)
 	{
 		for (gap = 1; gap * 3 + 1 < size; gap = gap * 3 + 1)
 			;
-		for (swp = 0; gap; gap = (gap - 1) / 3)
+		if (gap > 1)
+			print = 1;
+		for (; gap; gap = (gap - 1) / 3)
 		{
-			for (i = gap; i < size; ++i, array[j] = tmp)
-				for (j = i, tmp = array[i]; j >= gap && array[j - gap] > tmp; j -= gap)
-					if (array[j] != array[j - gap])
-					{
-						array[j] = array[j - gap];
-						swp = 1;
-					}
-			if (swp)
+			for (i = gap; i < size; ++i)
+			{
+				tmp = array[i];
+				for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+					array[j] = array[j - gap];
+				array[j] = tmp;
+			}
+			if (print)
 				print_array(array, size);
 		}
 	}
