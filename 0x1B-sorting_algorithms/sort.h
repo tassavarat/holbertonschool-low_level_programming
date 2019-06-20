9b3fc77a4c04638bdd497448a1d2f718d2bdc5e8
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-19 19:20:42 -0700

    sort.h update

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -27,5 +27,6 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
 #endif /* SORT_H */
