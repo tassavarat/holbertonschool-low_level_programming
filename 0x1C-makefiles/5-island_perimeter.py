78834b0284aaf58262f6285f67def4acb7c8af29
tassavarat <tassavarat@gmail.com>
2019-07-12 01:26:50 -0700

    Removed unnecessary +

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,11 +13,11 @@ def island_perimeter(grid):
         for j, n in enumerate(r):
             if n == 1:
                 if i == 0 or grid[i - 1][j] != 1:
-                    c += + 1
+                    c += 1
                 if j == 0 or grid[i][j - 1] != 1:
-                    c += + 1
+                    c += 1
                 if j == width or grid[i][j + 1] != 1:
-                    c += + 1
+                    c += 1
                 if i == length or grid[i + 1][j] != 1:
-                    c += + 1
+                    c += 1
     return c
