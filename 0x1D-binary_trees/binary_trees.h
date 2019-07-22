9aa1ce3f47c6eff78b578a720e0c3dd1d2a049c8
tassavarat <tassavarat@gmail.com>
2019-07-22 16:00:04 -0700

    Header update

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -32,5 +32,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
