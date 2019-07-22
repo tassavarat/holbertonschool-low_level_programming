f694079a5cdb18dd8ef716c2a7cb322f3b2d2fec
tassavarat <tassavarat@gmail.com>
2019-07-22 15:53:10 -0700

    First commit

---
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf
+ * @node: Pointer to node being checked
+ *
+ * Return: 1 if leaf, otherwise 0 if not or NULL
+ **/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node && !node->left && !node->right)
+		return (1);
+	return (0);
+}
