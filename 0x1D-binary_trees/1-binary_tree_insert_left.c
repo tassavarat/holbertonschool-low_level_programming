f1cd1b6137e7f8b3862d0b193d27322a1a4a9e1d
tassavarat <tassavarat@gmail.com>
2019-07-22 15:50:05 -0700

    Linter fixes and update header

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * Inserts a node as the left_child of another node
+ * binary_tree_insert_left - Inserts a node as the left_child of another node
  * @parent: Pointer to the node to insert the left-child in
  * @value: value to store in the new node
  *
  * Return: Pointer to created node, NULL on failure or if parent is NULL
- * */
+ **/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
@@ -22,5 +22,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = new;
 		return (new);
 	}
-	return NULL;
+	return (NULL);
 }
--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * Inserts a node as the right_child of another node
+ * binary_tree_insert_right - Inserts a node as the right_child of another node
  * @parent: Pointer to the node to insert the right-child in
  * @value: value to store in the new node
  *
  * Return: Pointer to created node, NULL on failure or if parent is NULL
- * */
+ **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
@@ -22,5 +22,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right = new;
 		return (new);
 	}
-	return NULL;
+	return (NULL);
 }
