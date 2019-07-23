ad31f744e4cd36e7613d69ced95bccde472c3f15
tassavarat <tassavarat@gmail.com>
2019-07-22 18:44:22 -0700

    Update header and implemented 10

---
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree: Pointer to node to measure depth
+ *
+ * Return: Depth of binary tree, 0 if NULL
+ **/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return (tree && tree->parent ? 1 + binary_tree_depth(tree->parent) : 0);
+}
--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -36,5 +36,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
