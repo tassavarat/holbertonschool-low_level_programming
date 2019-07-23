b12c1f8be093c2f77382d35091e8f6f20e7bde82
tassavarat <tassavarat@gmail.com>
2019-07-23 16:10:20 -0700

    Removed unnecessary function

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,22 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: Height of tree, 0 if NULL
- **/
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t height_l;
-	size_t height_r;
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l - height_r);;
-}
-
-/**
  * binary_tree_balance - Get balance factor of binary tree
  * @tree: Pointer to root of binary tree
  *
@@ -29,8 +13,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return 0;
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 	return (height_l - height_r);
 }
