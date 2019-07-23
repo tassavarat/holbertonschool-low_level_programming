d7f06dc3a3b12b1e7edfedbc6a54957f4bfd21ee
tassavarat <tassavarat@gmail.com>
2019-07-23 16:14:34 -0700

    Linter fixes

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -1,8 +1,14 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - Counts the nodes with at leastn 1 child in binary tree
+ * @tree: Pointer to root node of tree to count number of nodes
+ *
+ * Return: Numeber of nodes, 0 if tree is NULL or pointer isn't node
+ **/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree || (!tree->left && !tree->right))
-		return 0;
+		return (0);
 	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -12,7 +12,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	size_t height_r;
 
 	if (!tree)
-		return 0;
+		return (0);
 	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 	return (height_l - height_r);
