ea67311ed0a86a17c2714c2fa534e615166a8b3f
tassavarat <tassavarat@gmail.com>
2019-07-24 14:08:44 -0700

    Added return on NULL tree

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -11,6 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_l;
 	size_t height_r;
 
+	if (!tree)
+		return (0);
+
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (height_l > height_r ? height_l : height_r);
