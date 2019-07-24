040fff5dd19614c3f78cb086011d1e358f7708ee
jmtes <juliennetesoro@gmail.com>
2019-07-24 20:08:54 +0000

    Add prototype

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -45,5 +45,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
