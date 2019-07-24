3a52451226996d6dace8a3ff179008df395eacd9
jmtes <juliennetesoro@gmail.com>
2019-07-24 23:55:39 +0000

    Make betty compliant

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -32,6 +32,12 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
 
+/**
+ * binary_tree_is_perfect - Determine if binary tree is perfect
+ * @tree: Pointer to root of tree
+ *
+ * Return: 1 if tree is perfect, 0 otherwise
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int height, nodes;
