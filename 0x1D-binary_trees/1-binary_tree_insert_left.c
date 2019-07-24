697b3d7e9a9b508e971120216827bb5148f873f9
tassavarat <tassavarat@gmail.com>
2019-07-24 13:49:32 -0700

    Fixed memleak

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -11,17 +11,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (parent)
+	if (!parent)
+		return (NULL);
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
+
+	if (parent->left)
 	{
-		new = binary_tree_node(parent, value);
-			return (NULL);
-		if (parent->left)
-		{
-			parent->left->parent = new;
-			new->left = parent->left;
-		}
-		parent->left = new;
-		return (new);
+		parent->left->parent = new;
+		new->left = parent->left;
 	}
-	return (NULL);
+	parent->left = new;
+	return (new);
 }
