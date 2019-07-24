fafb25797956400afc2f454fde6b3799d04e7180
tassavarat <tassavarat@gmail.com>
2019-07-24 14:29:30 -0700

    Debug

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -16,5 +16,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	/* printf("tree->%d: %d - %d = %d\n", tree->n, height_l, height_r, height_l - height_r); */
 	return (height_l - height_r);
 }
