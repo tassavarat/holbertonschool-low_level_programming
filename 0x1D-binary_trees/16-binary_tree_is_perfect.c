b3d7e0d8844b642a82930c01f9f3cf0c205c22e9
tassavarat <tassavarat@gmail.com>
2019-07-24 16:19:20 -0700

    Complete rewrote

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -20,12 +20,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - Determine whether or not binary tree is perfect
+ * binary_tree_balance - Get balance factor of binary tree
  * @tree: Pointer to root of binary tree
  *
- * Return: 1 if binary tree is perfect, 0 otherwise
+ * Return: Balance factor of binary tree
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	int height_l;
 	int height_r;
@@ -35,5 +35,40 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l - height_r);
+}
+
+/**
+ * binary_tree_is_full - Check if binary tree is full
+ * @tree: Pointer to root of binary tree
+ *
+ * Return: 1 if tree is full, 0 otherwise
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int height_l;
+	int height_r;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_is_full(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_is_full(tree->right) : 0;
 	return (height_l == height_r);
+}
+/**
+ * binary_tree_is_perfect - Determine whether or not binary tree is perfect
+ * @tree: Pointer to root of binary tree
+ *
+ * Return: 1 if binary tree is perfect, 0 otherwise
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int bif, fl, fr;
+
+	bif = binary_tree_balance(tree);
+	fl = binary_tree_is_full(tree->left);
+	fr = binary_tree_is_full(tree->right);
+	return (!bif && fl == fr);
 }
