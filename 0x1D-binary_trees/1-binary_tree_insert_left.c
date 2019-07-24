5764cadf44f48f3333aeaf77be35ee9e099c2e0c
tassavarat <tassavarat@gmail.com>
2019-07-24 13:32:11 -0700

    Rewrote code

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -9,15 +9,16 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new, *tmp;
 
 	new = binary_tree_node(parent, value);
 	if (parent && new)
 	{
 		if (parent->left)
 		{
+			tmp = parent->left;
+			new->left = tmp;
 			parent->left->parent = new;
-			new->left = parent->left;
 		}
 		parent->left = new;
 		return (new);
