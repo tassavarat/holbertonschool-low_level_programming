f5017428ae41e2e5337d47c77c691f8076076e06
jmtes <juliennetesoro@gmail.com>
2019-07-25 21:24:30 +0000

    Account for tree balance

--- 0x1D-binary_trees/102-binary_tree_is_complete.c
+++ 0x1D-binary_trees/102-binary_tree_is_complete.c
@@ -1,6 +1,44 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of tree, -1 if NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
+
+/**
+ * binary_tree_balance - Get balance factor of binary tree
+ * @tree: Pointer to root of binary tree
+ *
+ * Return: Balance factor of binary tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int height_l;
+	int height_r;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l - height_r);
+}
+
+/**
  * binary_tree_sibling - Finds siblings of a node
  * @node: Pointer to node to find the sibling
  *
@@ -50,7 +88,8 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 		return (1);
 	else if (tree->left && !tree->right)
 		return (binary_tree_is_complete(tree->left));
-	return (binary_tree_is_complete(tree->left) &&
+	return ((binary_tree_balance(tree) >= 0) &&
+			binary_tree_is_complete(tree->left) &&
 			(binary_tree_is_complete(tree->right) &&
 			check_left_sib(tree->right)));
 }
