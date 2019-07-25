431c9385cc07d132a8c9c3f41fe4bfeeadd9b766
tassavarat <tassavarat@gmail.com>
2019-07-24 17:25:03 -0700

    Added conditional

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node && node->parent->parent)
+	if (node && node->parent && node->parent->parent)
 	{
 		if (node->parent->parent->left->n != node->parent->n)
 			return (node->parent->parent->left);
