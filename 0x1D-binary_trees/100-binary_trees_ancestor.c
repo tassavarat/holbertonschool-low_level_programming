03b5dc5bf8498a4320272ed6af3450ae5ee852f4
tassavarat <tassavarat@gmail.com>
2019-07-24 21:37:59 -0700

    Rewrote for collision

--- 0x1D-binary_trees/100-binary_trees_ancestor.c
+++ 0x1D-binary_trees/100-binary_trees_ancestor.c
@@ -1,6 +1,17 @@
 #include "binary_trees.h"
 
 /**
+ * _binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree: Pointer to node to measure depth
+ *
+ * Return: Depth of binary tree, 0 if NULL
+ */
+int _binary_tree_depth(const binary_tree_t *tree)
+{
+	return (tree && tree->parent ? 1 + _binary_tree_depth(tree->parent) : 0);
+}
+
+/**
  * binary_trees_ancestor - Finds lowest common ancestor of two nodes
  * @first: Pointer to first node
  * @second: Pointer to second node
@@ -11,21 +22,27 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	const binary_tree_t *node;
+	binary_tree_t *current_f, *current_s;
+	int diff;
 
-	if ((!first || !second))
+	if (!first || !second)
 		return (NULL);
+	diff = _binary_tree_depth(first) - _binary_tree_depth(second);
+	current_f = (binary_tree_t *)first;
+	current_s = (binary_tree_t *)second;
 
-	if (first->n < second->n)
-		node = first;
-	else if (first == second)
+	while (diff < 0)
 	{
-		if (first->parent->left && first->parent->left->n == first->n)
-			return (first->parent->left);
-		else if (first->parent->right && first->parent->right->n == first->n)
-			return (first->parent->right);
+		current_s = current_s->parent;
+		++diff;
 	}
+	while (diff)
+	{
+		current_f = current_f->parent;
+		--diff;
+	}
+	if (current_f == current_s)
+		return (current_f);
 	else
-		node = second;
-	return (node->parent);
+		return (current_f->parent);
 }
