44781cbbf8a9d81932bc43d6d0d3cc909582572f
tassavarat <tassavarat@gmail.com>
2019-08-27 23:59:56 -0700

    Removed additional sqrt call

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 	do {
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		i = j;
-		j += sqrt(size);
+		j += jump;
 		if (i >= size)
 			break;
 	} while (array[min(j, size) - 1] < value);
